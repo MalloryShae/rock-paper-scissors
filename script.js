96f2c96a51f0f53bb5e72b10cd98290dd4a750f4
Mallory Napier <mallorynapier@gmail.com>
2019-11-08 11:58:05 +0200

    Seperated score keeping function

--- script.js
+++ script.js
@@ -7,57 +7,62 @@ function capitalize(string){
   return caps;
 }
 
-    function computerPlay(){
-      let choices = ["rock", "paper", "scissors"];
-      let choice = choices[Math.floor(Math.random() * choices.length)]
-      return choice;
-    }
+function computerPlay(){
+  let choices = ["rock", "paper", "scissors"];
+  let choice = choices[Math.floor(Math.random() * choices.length)]
+  return choice;
+}
 
-    let playerScore = 0
-    let computerScore = 0
-    let tieScore = 0
+let playerScore = 0
+let computerScore = 0
+let tieScore = 0
 
-   let playRound = function(playerSelection, computerSelection){
-      let playerCaps = capitalize(playerSelection);
-      let computerCaps = capitalize(computerSelection);
-      let winning = "You win! "+playerCaps+ " beats " +computerSelection+"!";
-      let losing = "You lose! " + computerCaps + " beats "+ playerSelection + "!";
-      let tie = "It's a tie!"
-      if ((playerSelection === "rock" && computerSelection === "scissors") ||
-          (playerSelection  === "scissors" && computerSelection === "paper") ||
-          (playerSelection === "paper" && computerSelection === "rock")){
-        playerScore = playerScore + 1;
-        console.log(winning);
-        return winning;
-      } else if (playerSelection  === computerSelection){
-        tieScore = tieScore +1;
-        console.log(tie);
-        return tie;
-      } else {
-        computerScore = computerScore + 1;
-        console.log(losing);
-        return losing;
-      }
-    }
+let playRound = function(playerSelection, computerSelection){
+  let playerCaps = capitalize(playerSelection);
+  let computerCaps = capitalize(computerSelection);
+  let winning = "You win! "+playerCaps+ " beats " +computerSelection+"!";
+  let losing = "You lose! " + computerCaps + " beats "+ playerSelection + "!";
+  let tie = "It's a tie!"
+  if ((playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection  === "scissors" && computerSelection === "paper") ||
+      (playerSelection === "paper" && computerSelection === "rock")){
+    playerScore = playerScore + 1;
+    console.log(winning);
+    return winning;
+  } else if (playerSelection  === computerSelection){
+    tieScore = tieScore +1;
+    console.log(tie);
+    return tie;
+  } else {
+    computerScore = computerScore + 1;
+    console.log(losing);
+    return losing;
+  }
+}
 
 
-    function game(){
-      let rounds = 0
-      for (var i = 0; i < 5; i++){
-        let computerSelection = computerPlay();
-        let playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase()
-        playRound(playerSelection, computerSelection);
-        rounds = rounds + 1;
-        let scoreDisplay = "Player Wins: " + playerScore + " | Computer Wins: " +computerScore+ " | Ties: " + tieScore;
-        console.log(scoreDisplay);
-      }
-      if ((rounds === 5) && (playerScore > computerScore) && (playerScore > tieScore)){
-        console.log("Player Wins!")
-      } else if ((rounds === 5) && (computerScore > playerScore) && (computerScore > tieScore)){
-        console.log("Computer Wins!")
-      }else{
-        console.log("It's a tie!")
-      }
-      }
+function game(){
+  let rounds = 0
+  for (var i = 0; i < 5; i++){
+    let computerSelection = computerPlay();
+    let playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase()
+    playRound(playerSelection, computerSelection);
+    rounds = rounds + 1;
+    let scoreDisplay = "Player Wins: " + playerScore + " | Computer Wins: " +computerScore+ " | Ties: " + tieScore;
+    console.log(scoreDisplay);
+  }
+}
 
-game()
+function keepScore (){
+  if ((playerScore > computerScore) && (playerScore > tieScore)){
+    console.log("Player Wins!")
+  } else if ((computerScore > playerScore) && (computerScore > tieScore)){
+    console.log("Computer Wins!")
+  }else{
+    console.log("It's a tie!")
+  }
+}
+
+
+game();
+keepScore();
